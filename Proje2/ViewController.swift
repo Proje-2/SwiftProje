afd9bbef4af19d9081e246cce9569d00189aca11
Tubanur Türkmen <tubanurturkmen0310@gmail.com>
2025-02-19 15:21:43 +0300

    Initial Commit

---
+++ Proje2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proje2
+//
+//  Created by Tubanur Türkmen on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Proje2Tests/Proje2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Proje2Tests.swift
+//  Proje2Tests
+//
+//  Created by Tubanur Türkmen on 19.02.2025.
+//
+
+import Testing
+@testable import Proje2
+
+struct Proje2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
